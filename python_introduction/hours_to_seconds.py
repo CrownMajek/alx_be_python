9e0d2bb93ddf371d65cf59a8a35599183d280a17
majek ogunsakin <ogunsakinmajek@gmail.com>
2025-09-14 00:06:45 +0100

    add hours_to_seconds.py

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,4 +1,4 @@
 hours = 2
 seconds = 3600
-total_seconds = hours * seconds
-print("[] hour(s) is [] seconds=", hours * seconds)
+to_seconds = hours * 3600
+print("[] hour(s) is [] 3600=", hours * 3600)
