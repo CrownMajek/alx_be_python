09fc51f5218feb5e92fed59d69ad9ac26c4ce917
majek ogunsakin <ogunsakinmajek@gmail.com>
2025-10-25 03:58:10 +0100

    Add file

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,25 +2,24 @@ class Book:
     def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
-    def display_info(self):
-        print(f"Book: {self.title} by {self.author}")
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
 
 #derived class1
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
-    
-    def display_info(self):
-        print(f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB")
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB)"
 
    #derived class2 
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
-    def display_info(self):
-        print(f"PrintBook: {self.title} by {self.author}")
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 #composition Class
 
@@ -30,8 +29,7 @@ class Library:
 
     def add_book(self, book):
         self.books.append(book)
-        print(f"Added {book.title} to the library.")
         
     def list_books(self):
-        for book in self.books:
-            book.display_info()
+            for book in self.books:
+                print(f"{book}")
