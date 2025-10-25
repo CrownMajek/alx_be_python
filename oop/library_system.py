bf74d0cf9b662dc550415d88a077a870cfbec8c6
majek ogunsakin <ogunsakinmajek@gmail.com>
2025-10-25 03:20:25 +0100

    Add file

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,24 +2,25 @@ class Book:
     def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
-    def __str__(self):
-        return f"'{self.title}' by {self.author}"
+    def display_info(self):
+        print(f"Book: {self.title} by {self.author}")
 
 #derived class1
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
-    def __str__(self):
-        return f"'{self.title}' by {self.author} (EBook: {self.file_size})"
+    
+    def display_info(self):
+        print(f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB")
 
    #derived class2 
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
-    def __str__(self):
-        return f"'{self.title}' by {self.author} (PrintBook, {self.page_count})"
+    def display_info(self):
+        print(f"PrintBook: {self.title} by {self.author}")
 
 #composition Class
 
@@ -27,14 +28,10 @@ class Library:
     def __init__(self):
         self.books = []
 
-    def add_book(self, book: Book):
+    def add_book(self, book):
         self.books.append(book)
         print(f"Added {book.title} to the library.")
         
     def list_books(self):
-        if not self.books:
-            print("No books in the library.")
-        else:
-            print("Books in the library:")
-            for book in self.books:
-                print(f" - {book}")
+        for book in self.books:
+            book.display_info()
